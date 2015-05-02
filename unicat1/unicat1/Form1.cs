d10104f507ea24c9a3fed376c3f5c33f2b6a1915
haruko-ooishi <annie125@toki.waseda.jp>
2015-05-02 15:18:47 +0900

    道つくりました

--- unicat1/unicat1/Form1.cs
+++ unicat1/unicat1/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using InoueLab;
 
 
 namespace unicat1
@@ -19,7 +20,6 @@ namespace unicat1
             InitializeComponent();
             ////画像ファイルを読み込んで、Imageオブジェクトを作成する
             //System.Drawing.Image img = System.Drawing.Image.FromFile(@"\\SERVERFILE1\Common\ユニラブ\ユニラブ2015\素材\paneru.png");
-
             ////画像を表示する
             //pictureBox1.Image = img;
 
@@ -32,16 +32,26 @@ namespace unicat1
             //Image img = Image.FromFile(@"\\SERVERFILE1\Common\ユニラブ\ユニラブ2015\素材\paneru.png");
             int xmax=6,ymax=6;
             Image[,] img = new Image[xmax,ymax];
+            Image[,] img2 = new Image[xmax, ymax];
+            Image[,] fish = new Image[xmax, ymax];
+
             for (int i = 0; i < ymax ; i++)
             {
                 for (int j = 0; j < ymax; j++)
                 {
                     img[i, j] = Image.FromFile(@"\\SERVERFILE1\Common\ユニラブ\ユニラブ2015\素材\paneru.png");
-                    g.DrawImage(img[i, j], i * img[i, j].Width, j * img[i, j].Height, img[i, j].Width, img[i, j].Height);
+                    img2[i, j] = Image.FromFile(@"\\SERVERFILE1\Common\ユニラブ\ユニラブ2015\素材\paneru2.png");
+                    fish[i, j] = Image.FromFile(@"\\SERVERFILE1\Common\ユニラブ\ユニラブ2015\素材\fish.png");
+
+                    if (i % 2 == 0) g.DrawImage(img2[i, j], i * img2[i, j].Width, j * img2[i, j].Height, img2[i, j].Width, img2[i, j].Height);
+                    else            g.DrawImage(img[i, j], i * img[i, j].Width, j * img[i, j].Height, img[i, j].Width, img[i, j].Height);
+
                     img[i,j].Dispose();
                 }
    
             }
+
+            g.DrawImage(fish[1, 1], 1 * fish[1, 1].Width, 1 * fish[1, 1].Height, fish[1, 1].Width, fish[1, 1].Height);
 
                 //画像をcanvasの座標(20, 10)の位置に描画する
                 //g.DrawImage(img, 100, 10, img.Width, img.Height);
